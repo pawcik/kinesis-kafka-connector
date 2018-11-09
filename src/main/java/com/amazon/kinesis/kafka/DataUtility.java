f533e575dd15291c555130bc5c6d20e95b8b3908
pawel jedruch <pawcik@gmail.com>
2018-11-09 13:09:52 +0100

    Use jacoks to parse value to string and add record separator

--- src/main/java/com/amazon/kinesis/kafka/DataUtility.java
+++ src/main/java/com/amazon/kinesis/kafka/DataUtility.java
@@ -3,10 +3,13 @@ package com.amazon.kinesis.kafka;
 import java.io.UnsupportedEncodingException;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.Schema.Type;
 import org.apache.kafka.connect.data.Struct;
@@ -17,103 +20,113 @@ import com.amazonaws.services.kinesisfirehose.model.Record;
 
 public class DataUtility {
 
-	/**
-	 * Parses Kafka Values
-	 * 
-	 * @param schema
-	 *            - Schema of passed message as per
-	 *            https://kafka.apache.org/0100/javadoc/org/apache/kafka/connect/data/Schema.html
-	 * @param value
-	 *            - Value of the message
-	 * @return Parsed bytebuffer as per schema type
-	 */
-	public static ByteBuffer parseValue(Schema schema, Object value) {
-		Schema.Type t = schema.type();
-		switch (t) {
-		case INT8:
-			ByteBuffer byteBuffer = ByteBuffer.allocate(1);
-			byteBuffer.put((Byte) value);
-			return byteBuffer;
-		case INT16:
-			ByteBuffer shortBuf = ByteBuffer.allocate(2);
-			shortBuf.putShort((Short) value);
-			return shortBuf;
-		case INT32:
-			ByteBuffer intBuf = ByteBuffer.allocate(4);
-			intBuf.putInt((Integer) value);
-			return intBuf;
-		case INT64:
-			ByteBuffer longBuf = ByteBuffer.allocate(8);
-			longBuf.putLong((Long) value);
-			return longBuf;
-		case FLOAT32:
-			ByteBuffer floatBuf = ByteBuffer.allocate(4);
-			floatBuf.putFloat((Float) value);
-			return floatBuf;
-		case FLOAT64:
-			ByteBuffer doubleBuf = ByteBuffer.allocate(8);
-			doubleBuf.putDouble((Double) value);
-			return doubleBuf;
-		case BOOLEAN:
-			ByteBuffer boolBuffer = ByteBuffer.allocate(1);
-			boolBuffer.put((byte) ((Boolean) value ? 1 : 0));
-			return boolBuffer;
-		case STRING:
-			try {
-				return ByteBuffer.wrap(((String) value).getBytes("UTF-8"));
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				System.out.println("Message cannot be translated:" + e.getLocalizedMessage());
-			}
-		case ARRAY:
-			Schema sch = schema.valueSchema();
-			if (sch.type() == Type.MAP || sch.type() == Type.STRUCT) {
-				throw new DataException("Invalid schema type.");
-			}
-			Object[] objs = (Object[]) value;
-			ByteBuffer[] byteBuffers = new ByteBuffer[objs.length];
-			int noOfByteBuffer = 0;
+  public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-			for (Object obj : objs) {
-				byteBuffers[noOfByteBuffer++] = parseValue(sch, obj);
-			}
+  /**
+   * Parses Kafka Values
+   *
+   * @param schema
+   *            - Schema of passed message as per
+   *            https://kafka.apache.org/0100/javadoc/org/apache/kafka/connect/data/Schema.html
+   * @param value
+   *            - Value of the message
+   * @return Parsed bytebuffer as per schema type
+   */
+  public static ByteBuffer parseValue(Schema schema, Object value) {
+    Schema.Type t = schema.type();
+    switch (t) {
+      case INT8:
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1);
+        byteBuffer.put((Byte) value);
+        return byteBuffer;
+      case INT16:
+        ByteBuffer shortBuf = ByteBuffer.allocate(2);
+        shortBuf.putShort((Short) value);
+        return shortBuf;
+      case INT32:
+        ByteBuffer intBuf = ByteBuffer.allocate(4);
+        intBuf.putInt((Integer) value);
+        return intBuf;
+      case INT64:
+        ByteBuffer longBuf = ByteBuffer.allocate(8);
+        longBuf.putLong((Long) value);
+        return longBuf;
+      case FLOAT32:
+        ByteBuffer floatBuf = ByteBuffer.allocate(4);
+        floatBuf.putFloat((Float) value);
+        return floatBuf;
+      case FLOAT64:
+        ByteBuffer doubleBuf = ByteBuffer.allocate(8);
+        doubleBuf.putDouble((Double) value);
+        return doubleBuf;
+      case BOOLEAN:
+        ByteBuffer boolBuffer = ByteBuffer.allocate(1);
+        boolBuffer.put((byte) ((Boolean) value ? 1 : 0));
+        return boolBuffer;
+      case STRING:
+        try {
+          return ByteBuffer.wrap(((String) value).getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+          // TODO Auto-generated catch block
+          System.out.println("Message cannot be translated:" + e.getLocalizedMessage());
+        }
+      case ARRAY:
+        Schema sch = schema.valueSchema();
+        if (sch.type() == Type.MAP || sch.type() == Type.STRUCT) {
+          throw new DataException("Invalid schema type.");
+        }
+        Object[] objs = (Object[]) value;
+        ByteBuffer[] byteBuffers = new ByteBuffer[objs.length];
+        int noOfByteBuffer = 0;
 
-			ByteBuffer result = ByteBuffer.allocate(Arrays.stream(byteBuffers).mapToInt(Buffer::remaining).sum());
-			Arrays.stream(byteBuffers).forEach(bb -> result.put(bb.duplicate()));
-			return result;
-		case BYTES:
-			if (value instanceof byte[])
-				return ByteBuffer.wrap((byte[]) value);
-			else if (value instanceof ByteBuffer)
-				return (ByteBuffer) value;
-		case MAP:
-			// TO BE IMPLEMENTED
-			return ByteBuffer.wrap(null);
-		case STRUCT:
+        for (Object obj : objs) {
+          byteBuffers[noOfByteBuffer++] = parseValue(sch, obj);
+        }
 
-			List<ByteBuffer> fieldList = new LinkedList<ByteBuffer>();
-			// Parsing each field of structure
-			schema.fields().forEach(field -> fieldList.add(parseValue(field.schema(), ((Struct) value).get(field))));
-			// Initialize ByteBuffer
-			ByteBuffer processedValue = ByteBuffer.allocate(fieldList.stream().mapToInt(Buffer::remaining).sum());
-			// Combine bytebuffer of all fields
-			fieldList.forEach(buffer -> processedValue.put(buffer.duplicate()));
+        ByteBuffer result = ByteBuffer.allocate(Arrays.stream(byteBuffers).mapToInt(Buffer::remaining).sum());
+        Arrays.stream(byteBuffers).forEach(bb -> result.put(bb.duplicate()));
+        return result;
+      case BYTES:
+        if (value instanceof byte[])
+          return ByteBuffer.wrap((byte[]) value);
+        else if (value instanceof ByteBuffer)
+          return (ByteBuffer) value;
+      case MAP:
+        // TO BE IMPLEMENTED
+        return ByteBuffer.wrap(null);
+      case STRUCT:
 
-			return processedValue;
+        List<ByteBuffer> fieldList = new LinkedList<ByteBuffer>();
+        // Parsing each field of structure
+        schema.fields().forEach(field -> fieldList.add(parseValue(field.schema(), ((Struct) value).get(field))));
+        // Initialize ByteBuffer
+        ByteBuffer processedValue = ByteBuffer.allocate(fieldList.stream().mapToInt(Buffer::remaining).sum());
+        // Combine bytebuffer of all fields
+        fieldList.forEach(buffer -> processedValue.put(buffer.duplicate()));
 
-		}
-		return null;
-	}
+        return processedValue;
 
-	/**
-	 * Converts Kafka record into Kinesis record
-	 * 
-	 * @param sinkRecord
-	 *            Kafka unit of message
-	 * @return Kinesis unit of message
-	 */
-	public static Record createRecord(SinkRecord sinkRecord) {
-		return new Record().withData(parseValue(sinkRecord.valueSchema(), sinkRecord.value()));
-	}
+    }
+    return null;
+  }
+
+  /**
+   * Converts Kafka record into Kinesis record
+   *
+   * @param sinkRecord
+   *            Kafka unit of message
+   * @return Kinesis unit of message
+   */
+  static Record createRecord(SinkRecord sinkRecord) {
+    String recordSeparator = "\n";
+    try {
+      String valueAsString = OBJECT_MAPPER.writer().writeValueAsString(sinkRecord.value());
+      valueAsString = valueAsString + recordSeparator;
+      ByteBuffer bytebuffer = ByteBuffer.wrap(valueAsString.getBytes(StandardCharsets.UTF_8));
+      return new Record().withData(bytebuffer);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
 }
